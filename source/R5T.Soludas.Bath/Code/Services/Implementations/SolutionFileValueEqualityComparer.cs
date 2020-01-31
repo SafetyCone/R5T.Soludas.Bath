69385100c87c4f5d507dabf1dd0dbf38c812e4cb
MinexAutomation <minex.automation@gmail.com>
2020-01-31 00:01:25 -0800

    Added SolutionFileValueEqualityComparer.

---
+++ source/R5T.Soludas.Bath/Code/Services/Implementations/SolutionFileValueEqualityComparer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+using R5T.Bath;
+using R5T.Cambridge.Types;
+using R5T.Vandalia.Bath;
+
+
+namespace R5T.Soludas.Bath
+{
+    public class SolutionFileValueEqualityComparer : HumanOutputValueEqualityComparer<SolutionFile>
+    {
+        public SolutionFileValueEqualityComparer(IHumanOutput humanOutput)
+            : base(humanOutput)
+        {
+        }
+
+        public override bool Equals(SolutionFile x, SolutionFile y)
+        {
+            bool isFormatVersionEqual = x.FormatVersion == y.FormatVersion;
+
+            var isEqual = isFormatVersionEqual;
+
+            if(isEqual)
+            {
+                this.HumanOutput.WriteLine("Solution files NOT equal...");
+            }
+            else
+            {
+                this.HumanOutput.WriteLine("Solution files are equal.");
+            }
+
+            return isEqual;
+        }
+    }
+}
