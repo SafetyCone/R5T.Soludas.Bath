ef11b35ac78a547be46fff5045deb9517897dcb3
MinexAutomation <minex.automation@gmail.com>
2020-01-31 00:01:50 -0800

    Added IServiceCollection extensions.

---
+++ source/R5T.Soludas.Bath/Code/Services/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+using Microsoft.Extensions.DependencyInjection;
+
+using R5T.Bath;
+using R5T.Cambridge.Types;
+using R5T.Dacia;
+using R5T.Vandalia;
+
+
+namespace R5T.Soludas.Bath
+{
+    public static class IServiceCollectionExtensions
+    {
+        /// <summary>
+        /// Adds the <see cref="SolutionFileValueEqualityComparer"/> implementation of <see cref="IValueEqualityComparer{T}"/>, T: <see cref="SolutionFile"/>, as a <see cref="ServiceLifetime.Singleton"/>.
+        /// </summary>
+        public static IServiceCollection AddBathVisualStudioSolutionFileValueEqualityComparer(this IServiceCollection services,
+            ServiceAction<IHumanOutput> addHumanOutput)
+        {
+            services
+                .AddSingleton<IValueEqualityComparer<SolutionFile>, SolutionFileValueEqualityComparer>()
+                .RunServiceAction(addHumanOutput)
+                ;
+
+            return services;
+        }
+
+        /// <summary>
+        /// Adds the <see cref="SolutionFileValueEqualityComparer"/> implementation of <see cref="IValueEqualityComparer{T}"/>, T: <see cref="SolutionFile"/>, as a <see cref="ServiceLifetime.Singleton"/>.
+        /// </summary>
+        public static ServiceAction<IValueEqualityComparer<SolutionFile>> AddBathVisualStudioSolutionFileValueEqualityComparerAction(this IServiceCollection services,
+            ServiceAction<IHumanOutput> addHumanOutput)
+        {
+            var serviceAction = new ServiceAction<IValueEqualityComparer<SolutionFile>>(() => services.AddBathVisualStudioSolutionFileValueEqualityComparer(
+                addHumanOutput));
+            return serviceAction;
+        }
+    }
+}
